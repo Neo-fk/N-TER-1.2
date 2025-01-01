34eeddf0427e82a518efbe46f24abcff727973f1
Anuwa <81525953+Neo-fk@users.noreply.github.com>
2025-01-01 21:07:38 +0530

    Update disspace.js

--- disspace.js
+++ disspace.js
@@ -1,50 +1,85 @@
-const { Client, Collection } = require("discord.js");
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
 const { DisTube } = require('distube');
 const { SoundCloudPlugin } = require('@distube/soundcloud');
 const { SpotifyPlugin } = require('@distube/spotify');
+const { DeezerPlugin } = require('@distube/deezer');
+const { YouTubePlugin } = require('@distube/youtube');
+const { DirectLinkPlugin } = require('@distube/direct-link');
+const { YtDlpPlugin } = require('@distube/yt-dlp');
+const { AppleMusicPlugin } = require('distube-apple-music');
+const { TidalPlugin } = require('distube-tidal');
 
 class MainClient extends Client {
     constructor() {
-       super({
+        super({
             shards: "auto",
-            intents: 32767,
-            allowedMentions: {
-            parse: ["roles", "users", "everyone"],
-            repliedUser: false
-        },
+            allowedMentions: { parse: ["users", "roles"] },
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMembers,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.GuildVoiceStates,
+                GatewayIntentBits.MessageContent,
+            ],
+            partials: [Partials.Channel, Partials.Message]
+        });
+
+    process.on('unhandledRejection', error => console.log(error));
+    process.on('uncaughtException', error => console.log(error));
+
+    this.config = require('./settings/config.js');
+    this.owner = this.config.OWNER_ID;
+    this.color = this.config.EMBED_COLOR;
+    if (!this.token) this.token = this.config.TOKEN;
+
+    const client = this;
+
+    this.distube = new DisTube(client, {
+        savePreviousSongs: true,
+        emitAddListWhenCreatingQueue: true,
+        emitAddSongWhenCreatingQueue: true,
+        plugins: [
+            new TidalPlugin(),
+            new AppleMusicPlugin(),
+            new YtDlpPlugin({ update: false }),
+            new DirectLinkPlugin(),
+            new YouTubePlugin(),
+            new SoundCloudPlugin(),
+            new DeezerPlugin(),
+            checkSpotify(client)
+        ],
     });
 
-process.on('unhandledRejection', error => console.log(error));
-process.on('uncaughtException', error => console.log(error));
-
-this.config = require('./settings/config.js');
-this.prefix = this.config.PREFIX;
-this.owner = this.config.OWNER_ID;
-if(!this.token) this.token = this.config.TOKEN;
-
-const client = this;
-
-this.distube = new DisTube(client, {
-  searchSongs: 0, /// SET TO 5 FOR ENABLE SEARCH MODE!
-  searchCooldown: 30,
-  leaveOnEmpty: false,
-  emptyCooldown: 600,
-  leaveOnFinish: false,
-  leaveOnStop: false,
-  youtubeDL: false,
-  plugins: [
-  new SoundCloudPlugin(), 
-  new SpotifyPlugin({
-      emitEventsAfterFetching: true
-    })],
-});
-
-["aliases", "commands"].forEach(x => client[x] = new Collection());
-["loadCommands", "loadEvents", "loadDistube"].forEach(x => require(`./handlers/${x}`)(client));
+    ["slash"].forEach(x => client[x] = new Collection());
+    ["loadCommands", "loadEvents", "loadPlayer", "loadDatabase"].forEach(x => require(`./handlers/${x}`)(client));
 
     }
-        connect() {
-            return super.login(this.token);
+    connect() {
+        return super.login(this.token);
     };
 };
+
 module.exports = MainClient;
+
+function checkSpotify(client) {
+    if (client.config.SPOTIFY_TRACKS) {
+    //    console.log("[INFO] You're (Enabled) Spotify More Tracks Support!");
+        return spotifyOn(client);
+    } else {
+    //    console.log("[INFO] You're (Not Enabled) Spotify More Tracks Support!");
+        return spotifyOff();
+    }
+}
+
+function spotifyOn(client) {
+    return new SpotifyPlugin({
+        api: {
+            clientId: client.config.SPOTIFY_ID,
+            clientSecret: client.config.SPOTIFY_SECRET
+        }
+    })
+}
+
+function spotifyOff() {
+    return new SpotifyPlugin({})
+}
